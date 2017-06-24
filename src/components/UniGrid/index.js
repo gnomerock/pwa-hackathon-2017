10b50d087abe8120c24cd06a301e4973eab53fcd
nuttakorn buajarern <nuttakorn.buajarern@gmail.com>
2017-06-24 16:09:15 +0700

    update university list from realtime db

--- src/components/UniGrid/index.js
+++ src/components/UniGrid/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {GridList, GridTile} from 'material-ui/GridList';
 import IconButton from 'material-ui/IconButton';
+import Subheader from 'material-ui/Subheader';
 import StarBorder from 'material-ui/svg-icons/toggle/star-border';
+import * as firebase from 'firebase';
+import _ from 'underscore';
 
 const styles = {
   root: {
@@ -18,48 +21,55 @@ const styles = {
     paddingRight: 16
   },
 };
-const tilesData = [
-  {
-    img: 'http://www.chiangmaicitylife.com/wp-content/uploads/imported/citynews/1346878940.jpg',
-    title: 'ChiangMai University',
-    author: 'gnomerock',
-  },
-  {
-    img: 'http://www.chula.ac.th/wp-content/uploads/2014/02/banner-1.jpg',
-    title: 'Chulalongkorn University',
-    author: 'gnomerock',
-  },
-  {
-    img: 'http://www.manager.co.th/asp-bin/Image.aspx?ID=2528479',
-    title: 'Mahidol University',
-    author: 'gnomerock',
-  },
-  {
-    img: 'http://campus.sanook.com/story_picture/b/05077_002.jpg',
-    title: 'Kasetsart University',
-    author: 'gnomerock',
-  }
-];
 
-const UniGrid = (props) => (
-  <div style={styles.root}>
-      <GridList
-        cellHeight={180}
-        style={styles.gridList}
-      >
-        {/* <Subheader>December</Subheader> */}
-        {tilesData.map((tile) => (
-          <GridTile
-            key={tile.img}
-            title={tile.title}
-            subtitle={<span>by <b>{tile.author}</b></span>}
-            actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
-          >
-            <img src={tile.img} alt={tile.title}/>
-          </GridTile>
-        ))}
-      </GridList>
-    </div>
-)
+class UniGrid extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tilesData: []
+    }
+    this.database = firebase.database();
+  }
+
+  componentDidMount() {
+    this.database.ref('universities').on('value', (snap)=> {
+      const prepareTiles = [];
+      _.mapObject(snap.val(), (uni, key) => {
+        prepareTiles.push({
+          key: key,
+          name: uni.name,
+          description: uni.description,
+        })
+      })
+      this.setState({
+        tilesData: prepareTiles
+      })
+    })
+  }
+
+  render() {
+
+    return (
+      <div style={styles.root}>
+        <GridList
+          cellHeight={180}
+          style={styles.gridList}
+        >
+          <Subheader>General</Subheader>
+          {this.state.tilesData.map((tile) => (
+            <GridTile
+              key={tile.key}
+              title={tile.name}
+              subtitle={<span><b>{tile.description}</b></span>}
+              actionIcon={<IconButton><StarBorder color="white" /></IconButton>}
+            >
+              <img src={tile.name} alt={tile.name}/>
+            </GridTile>
+          ))}
+        </GridList>
+      </div>
+    )
+  }
+}
 
 export default UniGrid;
